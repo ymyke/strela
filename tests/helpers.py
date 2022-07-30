bf13f0411f6f33803f4e822511bab7fcf0aa0a20
ymyke <ymyke@users.noreply.github.com>
2022-07-30 17:57:25 +0200

    Add test helpers module

---
+++ tests/helpers.py
@@ -0,0 +1,13 @@
+"""Common helpers that are used in more than one test."""
+
+import pandas as pd
+
+
+def create_metric_history_df(allsame: bool = True):
+    # FIXME Use this also in double down alert tests?
+    df = pd.DataFrame(pd.date_range(start="01/01/2015", end="08/01/2020", tz="UTC"))
+    df["close"] = 1
+    df.set_index(0, inplace=True)
+    if not allsame:
+        df.loc["2020-01-01", "close"] = 100
+    return df
