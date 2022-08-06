9de0a563563b2827e649f95cdb95b5a842036e86
ymyke <ymyke@users.noreply.github.com>
2022-08-06 15:39:21 +0200

    Add AlertStateRepository including tests
    In order to factor out the repo from the alert generation code.

---
+++ strela/alertstates/alertstaterepository.py
@@ -0,0 +1,36 @@
+import glob
+import shutil
+from typing import Optional
+import os
+import shelve
+import slugify
+from . import AlertState
+
+
+class AlertStateRepository:
+
+    _FOLDER = "c:/code/strela/data/"  # FIXME Make configurable
+    _BACKUPFOLDER = os.path.join(_FOLDER, "backups")
+
+    def __init__(self, filename: str):
+        # self.filename = slugify.slugify(self.metricname + "-" + self.alertname)
+        self.filename = slugify.slugify(filename)
+        self._fullpath = os.path.join(self._FOLDER, self.filename)
+
+    def lookup_state(self, symbol_name: str) -> Optional[AlertState]:
+        """Look up ticker's state in shelf. Return None if nothing is found."""
+        try:
+            with shelve.open(self._fullpath) as shelf:
+                return shelf[symbol_name]
+        except KeyError:
+            return None
+
+    def update_state(self, symbol_name: str, state: AlertState) -> None:
+        """Update symbol's shelved state."""
+        with shelve.open(self._fullpath, writeback=True) as shelf:
+            shelf[symbol_name] = state
+
+    def backup(self):
+        """Move a copy of the shelf files to the backup folder."""
+        for file in glob.glob(self._fullpath + "*"):
+            shutil.copy(file, self._BACKUPFOLDER)
---
+++ tests/alertstates/test_alertstaterepository.py
@@ -0,0 +1,36 @@
+"""Tests for the `AlertStateRepository` class"""
+
+# pylint: disable=missing-function-docstring
+
+import pytest
+from strela.alertstates import AlertStateRepository, FluctulertState
+from tests.helpers import create_metric_history_df
+
+
+@pytest.fixture(autouse=True)
+def patch_shelveloc(tmpdir):
+    """Set temp directory for the repo so we don't override the production data."""
+    # pylint: disable=protected-access
+    saved_loc = AlertStateRepository._FOLDER
+    AlertStateRepository._FOLDER = tmpdir
+    yield
+    AlertStateRepository._FOLDER = saved_loc
+
+
+def test_init():
+    repo = AlertStateRepository("t*e#s?t-te st")
+    assert repo.filename == "t-e-s-t-te-st"
+    assert repo._fullpath.endswith("t-e-s-t-te-st")  # pylint: disable=protected-access
+
+
+def test_lookup_non_existing_symbol():
+    repo = AlertStateRepository("reponame")
+    assert repo.lookup_state("X") is None
+
+
+def test_update_and_lookup_existing_symbol():
+    repo = AlertStateRepository("reponame")
+    assert repo.lookup_state("X") is None
+    state = FluctulertState(create_metric_history_df())
+    repo.update_state("X", state)
+    assert state.eq(repo.lookup_state("X"))  # FIXME Type issue
