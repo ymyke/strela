b5789313b4a611c80f2c2ab72d6c8e241e924d34
ymyke <ymyke@users.noreply.github.com>
2022-08-08 11:11:01 +0200

    Simplify, improve structure, clarify here and there

--- strela/runner.py
+++ strela/runner.py
@@ -7,13 +7,15 @@ from strela.alertstates import (
     FluctulertState,
 )
 
+# FIXME Add mailing functionality
+
 # FIXME Add HTMLTemplate here
 # Use the below code but delete that subclass.
 class ExtendedStringTemplate(AlertToStringTemplate):
     def apply(self, symbol: SymbolType, *args, **kwargs) -> str:
         res = super().apply(symbol, *args, **kwargs).rstrip()
         try:
-            res += f"Strategy: {symbol.get_strategy_string()}\n"  # type: ignore
+            res += f"\nStrategy: {symbol.get_strategy_string()}\n"  # type: ignore
         except AttributeError:
             pass
         return res
@@ -27,47 +29,76 @@ sc.load_yaml("c:/code/fignal/mysymbols.yaml")  # FIXME Where to configure this?
 crypto_symbols = [x for x in sc.symbols if x.watch and x.type_ == "crypto"]
 stockx_symbols = [x for x in sc.symbols if x.watch and x.type_ != "crypto"]
 
-# Set up 2 categories of alert sets:
-category_list = [
-    (crypto_symbols, "Crypto", "https://www.coingecko.com/en/coins/{symbol.name}"),
-    (stockx_symbols, "Stockx", "https://www.google.com/search?q={symbol.name}+stock"),
+# Blueprints for links to more information:
+COINGECKO_URL = "https://www.coingecko.com/en/coins/{symbol.name}"
+GOOGLE_URL = "https://www.google.com/search?q={symbol.name}+stock"
+
+
+# Set up the list of all the alert categories:
+the_alert_list = [
+    #
+    # Every weekday: Check for double down alerts on both lists:
+    #
+    (
+        stockx_symbols,  # Symbols to check
+        "Stockx",  # Category name
+        "DoubleDownAlert",  # Alert name
+        DoubleDownAlertState,  # Alert state class
+        range(0, 5),  # Weekdays to check
+        GOOGLE_URL,  # Link pattern
+    ),
+    (
+        crypto_symbols,
+        "Crypto",
+        "DoubleDownAlert",
+        DoubleDownAlertState,
+        range(0, 5),
+        COINGECKO_URL,
+    ),
+    #
+    # Every mon and thu: Check for fluctulerts on both lists:
+    #
+    (
+        stockx_symbols,
+        "Stockx",
+        "Fluctulert",
+        FluctulertState,
+        [0, 3],
+        GOOGLE_URL,
+    ),
+    (
+        crypto_symbols,
+        "Crypto",
+        "Fluctulert",
+        FluctulertState,
+        [0, 3],
+        COINGECKO_URL,
+    ),
 ]
 
-# ----- DoubleDownAlerts -----
-# Every weekday: Check for double down alerts on both lists:
-if datetime.datetime.today().weekday() in range(0, 5):
-    for symbols, category_name, link_pattern in category_list:
-        template = ExtendedStringTemplate(  # FIXME Use HTML template
-            category_name, "DoubleDownAlert", "Price", link_pattern
-        )
-        repo = AlertStateRepository(f"{category_name}-price-ddalerts")
-        alerts = generate_alerts(
-            alertstate_class=DoubleDownAlertState,
-            metric_history_callback=lambda symbol: symbol.price_history().df,
-            symbols=symbols,
-            template=template,
-            repo=repo,
-        )
-        print(template.get_title())
-        print()
-        print(alerts)
-
-
-# ----- Fluctulerts -----
-# Every mon and thu: Check for fluctulerts on both lists:
-if datetime.datetime.today().weekday() in [0, 3]:
-    for symbols, category_name, link_pattern in category_list:
-        template = ExtendedStringTemplate(  # FIXME Use HTML template
-            category_name, "Fluctulert", "Price", link_pattern
-        )
-        repo = AlertStateRepository(f"{category_name}-price-fluctulerts")
-        alerts = generate_alerts(
-            alertstate_class=FluctulertState,
-            metric_history_callback=lambda symbol: symbol.price_history().df,
-            symbols=symbols,
-            template=template,
-            repo=repo,
-        )
-        print(template.get_title())
-        print()
-        print(alerts)
+# Do the real work:
+METRIC = "Price"
+for (
+    symbols,
+    category_name,
+    alert_name,
+    alert_class,
+    dayofweeks,
+    link_pattern,
+) in the_alert_list:
+    if datetime.datetime.today().weekday() not in dayofweeks:
+        continue
+    template = ExtendedStringTemplate(  # FIXME Use HTML template
+        category_name, alert_name, METRIC, link_pattern
+    )
+    repo = AlertStateRepository(f"{category_name}-{METRIC}-{alert_name}")
+    alerts = generate_alerts(
+        alertstate_class=alert_class,
+        metric_history_callback=lambda symbol: symbol.price_history().df,
+        symbols=symbols,
+        template=template,
+        repo=repo,
+    )
+    print(template.get_title())
+    print()
+    print(alerts)
