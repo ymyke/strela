c9e2ab759a9fdd0985d7132e67acb2b843bd5c86
ymyke <ymyke@users.noreply.github.com>
2022-08-10 19:23:58 +0200

    Add documentation

--- strela/templates.py
+++ strela/templates.py
@@ -1,12 +1,23 @@
+"""Templates that turn alerts into strings.
+
+Notes:
+
+- Currently, alerts are already barebone strings, and these templates rather "embellish"
+  them.
+- There are no specific tests for this module bc other tests also implicitly test the
+  templates.
+"""
+
 from typing import Optional
 from strela.symboltype import SymbolType
 from strela.alertstates import AlertState
 
-# FIXME Add tests
-# FIXME Add documentation
-
 
 class AlertToTextTemplate:
+    """Template to turn alert into text string. Also serves as the base class for other
+    templates.
+    """
+
     def __init__(
         self,
         category_name: str,
@@ -14,13 +25,24 @@ class AlertToTextTemplate:
         metric_name: str,
         link_pattern: str = "",
     ):
+        """`AlertToTextTemplate` initializer.
+
+        Args:
+        - category_name: Alert category, e.g. "Crypto".
+        - alert_name: Alert name, e.g. "Fluctulert".
+        - metric_name: Metric name, e.g. "Price".
+        - link_pattern: Link pattern, will be extrapolated in `apply`, e.g.
+          "https://www.google.com/search?q={symbol.name}+stock"
+
+        All the names are informational only and have no functional purpose.
+        """
         self.category_name = category_name
         self.alert_name = alert_name
         self.metric_name = metric_name
         self.link_pattern = link_pattern
-        # e.g. "https://www.google.com/search?q={symbol.name}+stock" FIXME add to doc
 
     def get_title(self) -> str:
+        """Returns the title/subject of the alert."""
         return f"📈🚨📉 {self.category_name} {self.metric_name} {self.alert_name}"
 
     def apply(
@@ -30,6 +52,14 @@ class AlertToTextTemplate:
         old_state: Optional[AlertState],
         latest_value: float,
     ) -> str:
+        """Extrapolates the template into a string and returns it.
+
+        Args:
+        - symbol: Symbol for which the alert is being generated.
+        - alert_state: Current state of the alert.
+        - old_state: Previous state of the alert.
+        - latest_value: Latest value of the metric.
+        """
         return f"""\
 {symbol.name} ⚠lert
 {alert_state.textify(old_state).rstrip()}
@@ -39,6 +69,8 @@ Latest {self.metric_name}: {latest_value}
 
 
 class AlertToHtmlTemplate(AlertToTextTemplate):
+    """Template subclass that generates HTML."""
+
     def apply(
         self,
         symbol: SymbolType,
@@ -46,6 +78,7 @@ class AlertToHtmlTemplate(AlertToTextTemplate):
         old_state: AlertState,
         latest_value: float,
     ) -> str:
+        """Extrapolates the template into HTML and returns it."""
         return f"""\
 <a href="{self.link_pattern.format(symbol=symbol)}">{symbol.name} ⚠lert</a>
 {alert_state.htmlify(old_state).rstrip()}
@@ -53,6 +86,7 @@ Latest {self.metric_name}: {latest_value}
 """
 
     def wrap_body(self, body: str) -> str:
+        """Wraps the alert in HTML wrapper tags."""
         return f"""\
 <pre><font face="Consolas, Lucida Console, Fira Code, Courier New, Courier, monospace">
 {body}
