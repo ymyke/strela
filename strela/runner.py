e476378e36e521745d7bde2d2b5280eb37aa1cca
ymyke <ymyke@users.noreply.github.com>
2022-08-10 07:46:16 +0200

    Add tests for runner script and revamp runner to make it testable

--- strela/runner.py
+++ strela/runner.py
@@ -38,6 +38,11 @@ ENABLE_ALL_DOWS = False
 FROM_EMAIL = "michael.naef@gmail.com"  # FIXME Remove my mail address
 TO_EMAIL = FROM_EMAIL
 
+# Blueprints for links to more information:
+# FIXME Could this be a tessa.symbol.Symbol concern?
+COINGECKO_URL = "https://www.coingecko.com/en/coins/{symbol.name}"
+GOOGLE_URL = "https://www.google.com/search?q={symbol.name}+stock"
+
 
 # Customized template derived from default HTML template:
 class MyAlertToHtmlTemplate(AlertToHtmlTemplate):
@@ -51,88 +56,93 @@ class MyAlertToHtmlTemplate(AlertToHtmlTemplate):
         )
 
 
-# Prepare the symbol lists:
-sc = SymbolCollection(symbol_class=ExtendedSymbol)
-sc.load_yaml(SYMBOLS_FILE)
-crypto_symbols = [x for x in sc.symbols if x.watch and x.type_ == "crypto"]
-stockx_symbols = [x for x in sc.symbols if x.watch and x.type_ != "crypto"]
+def run() -> None:
 
-# Blueprints for links to more information:
-# FIXME Could this be a tessa.symbol.Symbol concern?
-COINGECKO_URL = "https://www.coingecko.com/en/coins/{symbol.name}"
-GOOGLE_URL = "https://www.google.com/search?q={symbol.name}+stock"
+    # Prepare the symbol lists:
+    sc = SymbolCollection(symbol_class=ExtendedSymbol)
+    sc.load_yaml(SYMBOLS_FILE)
+    crypto_symbols = [x for x in sc.symbols if x.watch and x.type_ == "crypto"]
+    stockx_symbols = [x for x in sc.symbols if x.watch and x.type_ != "crypto"]
 
+    # Set up the list of all the alert categories:
+    the_alert_list = [
+        #
+        # Every weekday: Check for double down alerts on both lists:
+        #
+        (
+            stockx_symbols,  # Symbols to check
+            "Stockx",  # Category name
+            "DoubleDownAlert",  # Alert name
+            DoubleDownAlertState,  # Alert state class
+            range(0, 5),  # Weekdays to check
+            GOOGLE_URL,  # Link pattern
+        ),
+        (
+            crypto_symbols,
+            "Crypto",
+            "DoubleDownAlert",
+            DoubleDownAlertState,
+            range(0, 5),
+            COINGECKO_URL,
+        ),
+        #
+        # Every mon and thu: Check for fluctulerts on both lists:
+        #
+        (
+            stockx_symbols,
+            "Stockx",
+            "Fluctulert",
+            FluctulertState,
+            [0, 3],
+            GOOGLE_URL,
+        ),
+        (
+            crypto_symbols,
+            "Crypto",
+            "Fluctulert",
+            FluctulertState,
+            [0, 3],
+            COINGECKO_URL,
+        ),
+    ]
 
-# Set up the list of all the alert categories:
-the_alert_list = [
-    #
-    # Every weekday: Check for double down alerts on both lists:
-    #
-    (
-        stockx_symbols,  # Symbols to check
-        "Stockx",  # Category name
-        "DoubleDownAlert",  # Alert name
-        DoubleDownAlertState,  # Alert state class
-        range(0, 5),  # Weekdays to check
-        GOOGLE_URL,  # Link pattern
-    ),
-    (
-        crypto_symbols,
-        "Crypto",
-        "DoubleDownAlert",
-        DoubleDownAlertState,
-        range(0, 5),
-        COINGECKO_URL,
-    ),
-    #
-    # Every mon and thu: Check for fluctulerts on both lists:
-    #
-    (
-        stockx_symbols,
-        "Stockx",
-        "Fluctulert",
-        FluctulertState,
-        [0, 3],
-        GOOGLE_URL,
-    ),
-    (
-        crypto_symbols,
-        "Crypto",
-        "Fluctulert",
-        FluctulertState,
-        [0, 3],
-        COINGECKO_URL,
-    ),
-]
-
-# Do the real work:
-METRIC = "Price"
-for (
-    symbols,
-    category_name,
-    alert_name,
-    alert_class,
-    dayofweeks,
-    link_pattern,
-) in the_alert_list:
-    if datetime.datetime.today().weekday() not in dayofweeks and not ENABLE_ALL_DOWS:
-        continue
-    template = MyAlertToHtmlTemplate(category_name, alert_name, METRIC, link_pattern)
-    repo = AlertStateRepository(f"{category_name}-{METRIC}-{alert_name}")
-    alerts = generate_alerts(
-        alertstate_class=alert_class,
-        metric_history_callback=lambda symbl: symbl.price_history().df,  # type: ignore
-        symbols=symbols,
-        template=template,
-        repo=repo,
-    )
-    alerts_str = "\n".join(alerts)  # pylint: disable=invalid-name
-    if NO_MAIL:
-        print(template.get_title() + "\n" + alerts_str)
-    elif alerts:
-        mailer.mail(
-            from_=FROM_EMAIL,
-            to_=TO_EMAIL,
-            subject=template.get_title(),
-            body=template.wrap_body(alerts_str),
+    # Do the actual work:
+    metric = "Price"
+    for (
+        symbols,
+        category_name,
+        alert_name,
+        alert_class,
+        dayofweeks,
+        link_pattern,
+    ) in the_alert_list:
+        if (
+            datetime.datetime.today().weekday() not in dayofweeks
+            and not ENABLE_ALL_DOWS
+        ):
+            continue
+        template = MyAlertToHtmlTemplate(
+            category_name, alert_name, metric, link_pattern
         )
+        repo = AlertStateRepository(f"{category_name}-{metric}-{alert_name}")
+        alerts = generate_alerts(
+            alertstate_class=alert_class,
+            metric_history_callback=lambda s: s.price_history().df,  # type: ignore
+            symbols=symbols,
+            template=template,
+            repo=repo,
+        )
+        alerts_str = "\n".join(alerts)  # pylint: disable=invalid-name
+        if NO_MAIL:
+            print(template.get_title() + "\n" + alerts_str)
+        elif alerts:
+            mailer.mail(
+                from_=FROM_EMAIL,
+                to_=TO_EMAIL,
+                subject=template.get_title(),
+                body=template.wrap_body(alerts_str),
+            )
+
+
+if __name__ == "__main__":
+    run()
---
+++ tests/test_runner.py
@@ -0,0 +1,62 @@
+"""Orchestration/integration tests"""
+
+# pylint: disable=unused-import, no-member, missing-function-docstring, unused-argument
+
+import pytest
+import yagmail
+from tessa.price import PriceHistory
+import strela.runner as runner
+from .alertstates.test_alertstaterepository import patch_shelveloc
+from .helpers import create_metric_history_df
+
+# FIXME Rename according to name of runner in the end.
+
+TEST_EMAIL = "983409832498348938@mailinator.com"
+# FIXME Use user's mail address once we have proper config in place?
+
+
+@pytest.fixture(name="prepare_environment")
+def fixture_prepare_environment(tmp_path):
+    # Prepare environment:
+    file = tmp_path / "symbols.yaml"
+    file.write_text(
+        """\
+gmi:
+  type_: crypto
+  query: bankless-defi-innovation-index
+  strategy: [HoldForGrowth, HoldForDiversification]
+  watch: True
+  jurisdiction: unknown
+"""
+    )
+    runner.SYMBOLS_FILE = file
+    runner.ENABLE_ALL_DOWS = True
+    runner.TO_EMAIL = TEST_EMAIL
+
+
+def test_price_run(mocker, prepare_environment):
+    mocker.patch(
+        "tessa.symbol.Symbol.price_history",
+        return_value=PriceHistory(create_metric_history_df(allsame=False), "USD"),
+    )
+    mocker.patch("yagmail.SMTP")
+
+    # Run:
+    runner.run()
+
+    # Check:
+    yagmail.SMTP.assert_called()  # type: ignore
+    yagmail.SMTP().send.assert_any_call(
+        to=TEST_EMAIL,
+        subject="📈🚨📉 Crypto Price Fluctulert",
+        contents=mocker.ANY,
+    )
+    assert "gmi" in str(yagmail.SMTP().send.call_args)
+
+
+@pytest.mark.net  # FIXME Register somewhere
+def test_mailingalert_mail_real(prepare_environment):
+    """Will send mail to `TEST_EMAIL` address at mailinator.com. You can verify the
+    arrival of the mail there.
+    """
+    runner.run()
