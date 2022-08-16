08e31bb33d2f0655944ec8b32731d9d5379b5bf9
ymyke <ymyke@users.noreply.github.com>
2022-08-16 07:30:13 +0200

    Bump version; add standard imports

--- strela/__init__.py
+++ strela/__init__.py
@@ -1,1 +1,8 @@
-__version__ = '0.1.0'
+__version__ = "0.1.1"
+
+from . import config
+from . import alertstates
+from . import templates
+from .alert_generator import generate_alerts
+from .mailer import mail
+from . import my_runner
