2c091f50ca723b2df9e31d889755a31a636637d4
ymyke <ymyke@users.noreply.github.com>
2022-08-17 22:52:24 +0200

    Improve documentation

--- strela/alertstates/alertstate.py
+++ strela/alertstates/alertstate.py
@@ -1,4 +1,4 @@
-"""AlertState abtract base class"""
+"""AlertState ABC"""
 
 from __future__ import annotations
 from typing import Optional
@@ -7,15 +7,19 @@ from pandas import DataFrame
 
 
 class AlertState(ABC):
-    """ABC for all alert state classes."""
+    """The abstract base class for all alert states. Alert states encapsulate the logic
+    to determine whether an alert has triggered or not.
+    """
 
     @abstractmethod
     def __init__(self, hist: DataFrame) -> None:
-        """Mandatory init method that takes a history dataframe."""
+        """Constructor. Takes a history dataframe `hist`. The dataframe must have
+        timestamps as the index and exactly one column with the metric.
+        """
 
     @abstractmethod
     def textify(self, other: Optional[AlertState] = None) -> str:
-        """Return state as a text. Highlight differences to other if not None. Returns
+        """Return state as a text. Highlight differences to `other` if not None. Returns
         an empty string if nothing happened that would trigger an alert.
         """
 
@@ -25,12 +29,12 @@ class AlertState(ABC):
 
     @abstractmethod
     def eq(self, other: Optional[AlertState]) -> bool:
-        """Check for equality of this alert and other."""
+        """Check for equality of this alert and `other`. Note that `other` can be None."""
 
     @abstractmethod
     def is_ringing(self) -> bool:
-        """Return True if this alert has been triggered just now (meaning in the last
-        period).
+        """Return `True` if this alert has been triggered (meaning in the last
+        period) and not cooled down yet.
         """
 
     def __str__(self) -> str:
--- strela/alertstates/doubledownalertstate.py
+++ strela/alertstates/doubledownalertstate.py
@@ -1,54 +1,64 @@
+"""Double-down alert state."""
+
 from __future__ import annotations
 from collections import namedtuple
-from typing import Optional
+from typing import Optional, ClassVar, List
 import statistics
 from pandas import DataFrame
 from . import AlertState
 
 
 class Level(namedtuple("Level", ["trigger", "factor"])):
-    """Namedtuple subclass that overrides > operator to make things easier below."""
+    """A Level is simply a combination of trigger and factor, e.g., (0.2, 4), which
+    would mean: if the metric declines by >= 20%, suggest a 4x invest. Level is a
+    namedtuple subclass that overrides > operator to make things easier below.
+    """
 
-    def __gt__(self, other):
+    def __gt__(self, other: Level) -> bool:
         return other is None or self.trigger > other.trigger
 
 
 class DoubleDownAlertState(AlertState):
-    """Concrete class for double-down alert states.
-
-    Always keeps the current alert level including the date.
-
-    Attributes:
-    - currentlevel: The current alert level that is active. (Which does not necessarily
-      mean that it has been activated just now. Check alertactivated for that.) None, if
-      no level is active.
-    - alertactivated: Records whether a new alert level has been reached by the latest
-      history entry
-    - alerthistory: History of all the alerts that have been triggered
-
-    Settings / class attributes:
-    - levels: The different levels at which alerts are triggered including the
-      double-down factors for each level
-    - averagingperiod: Number of days to average over when determining if an alert
-      triggers.
-    - cooldownperiod: After this number of days the alert resets to 0 if no new alert
-      level has been reached.
+    """Concrete class for double-down alert states, which trigger for significant
+    downward movements and suggest over-proportional buys.
 
     Note that -- due to the way this function is implemented -- days is not the same as
     dates. Days is more like trading days because there is usually no weekend data in
     the dataframes that are fed into the initiator.
     """
 
-    # Class attributes:
-    levels = [  # FIXME Should be configurable somewhere
-        Level(trigger=0.1, factor=2),
+    # Class variables:
+
+    levels: ClassVar[List[Level]] = [
+        Level(trigger=0.1, factor=2),  # FIXME Levels should be configurable somewhere
         Level(trigger=0.2, factor=4),
         Level(trigger=0.3, factor=6),
         Level(trigger=0.4, factor=8),
         Level(trigger=0.5, factor=10),
     ]
-    averagingperiod = 30
-    cooldownperiod = 30
+    """The different levels at which alerts are triggered including the double-down
+    factors for each level."""
+
+    averagingperiod: ClassVar[int] = 30
+    """Number of days to average over when determining if an alert triggers."""
+
+    cooldownperiod: ClassVar[int] = 30
+    """After this number of days the alert resets to 0 if no new alert level has been
+    reached."""
+
+    # Instance variables:
+
+    currentlevel: Optional[Level]
+    """The current alert level that is active. (Which does not necessarily mean that
+    it has been activated just now. Check alertactivated for that.) None, if no level is
+    active."""
+
+    alertactivated: bool
+    """Records whether a new alert level has been reached by the latest history
+    entry."""
+
+    alerthistory: list
+    """History of all the alerts that have been triggered."""
 
     def __init__(self, hist: DataFrame) -> None:
         super().__init__(hist)
@@ -58,7 +68,7 @@ class DoubleDownAlertState(AlertState):
         self._scan_for_alerts(hist)
 
     def _scan_for_alerts(self, hist: DataFrame) -> Optional[Level]:
-        """Scan the entire history hist for alerts and set up the corresponding
+        """Scan the entire history `hist` for alerts and set up the corresponding
         attributes (alertactivated, alerthistory, currentlevel) in the object.
         """
 
